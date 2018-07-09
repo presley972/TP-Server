e928861090f629fee0070e20c6e53cb7a6851cc2
Presley Lupon <presleylupon@gmail.com>
2018-07-09 22:06:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Presley Lupon
+
+
+J'ai grave galéré j'ai suivi plein de tutoriel mais bon ce n'est pas encore convaincant
---
+++ parti1/src/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: presleylupon
+ * Date: 09/07/2018
+ * Time: 19:55
+ */
+
+$title = 'Site d\'exemple 1';?>
+<!Doctype>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?= $title ?></title>
+</head>
+<body>
+<h1><?= $title ?></h1>
+</body></html>
